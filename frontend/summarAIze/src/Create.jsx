9ec334772e4540de2280d44d56275e17c12de76c
zesponge <sumario82@gmail.com>
2024-02-15 23:36:15 -0500

    start server

---
+++ frontend/summarAIze/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div>
+      <input type="text" name=''/>
+      <button type='button'>Summarize</button>
+    </div>
+  )
+}
+
+export default Create
