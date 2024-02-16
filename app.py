43736a05bbaf3720e94c5e6f65eaaea8ac0b46bd
zesponge <sumario82@gmail.com>
2024-02-15 21:54:11 -0500

    change

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 from flask import Flask, request
-from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import pipeline
+from youtube_transcript_api import YouTubeTranscriptApi
+
 
 app = Flask(__name__)
 
-@app.get('/summary')
-def summary_api():
+@app.get('/summarize')
+def summarize_api():
     url = request.args.get('url', '')
     video_id = url.split('=')[1]
-    summary = get_summary(get_transcript(video_id))
-    # summariser = pipeline('summarization')
-    # video_id = "z8aBZZnv6y8"
-    # transcript = get_transcript(video_id)
-    # summary = summariser(transcript, max_length=100, min_length=30, do_sample=False)
-    # print(summary[0]['summary_text'])
+    transcript = get_transcript(video_id)
+    summary = get_summary(transcript)
     return summary, 200
 
 def get_transcript(video_id):
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-    transcript = ' '.join([d['text'] for d in transcript_list])
+    transcript_disjointed = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ' '.join([d['text'] for d in transcript_disjointed])
     return transcript
 
 def get_summary(transcript):
--- extension/popup.js
+++ extension/popup.js
@@ -5,7 +5,7 @@ btn.addEventListener("click", function() {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         var url = tabs[0].url;
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
+        xhr.open("GET", "http://127.0.0.1:5000/summarize?url=" + url, true);
         xhr.onload = function() {
             var text = xhr.responseText;
             const p = document.getElementById("output");
