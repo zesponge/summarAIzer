39503a1010c2bb7727aca40f3f36e218a82a7ac0
zesponge <sumario82@gmail.com>
2024-02-16 16:17:30 -0500

    front end storing in db

--- frontend/summaraizer/src/App.js
+++ frontend/summaraizer/src/App.js
@@ -1,23 +1,34 @@
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [itemText, setItemText] = useState('')
+
+  const addItem = async (e) => {
+    e.preventDefault()
+    try{
+      const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+      console.log(res)
+    } catch(err) {
+      console.log(err)
+    }
+
+  }
+
+
   return (
     <div className="App">
-      <form>
-        <input type="text" placeholder="enter URL"/>
-        <button type="submit">Summarize</button>
-      </form>
-      <div className="todo-listItems">
-        <div className="todo-item">
-          <p>this is item 1</p>
-          <button>update</button>
-          <button>delete</button>
-        </div>
-        <div className="todo-item">
-          <p>this is item 1</p>
-          <button>update</button>
-          <button>delete</button>
-        </div>
+      <div className='header'>
+        <h1>Summar<span className='black'>AI</span>zer</h1>
+      </div>
+      <div className='body'>
+        <form className='formclass' onSubmit={e => addItem(e)}>
+          <input className="inputbox" type='text' placeholder='enter URL' onChange={e => {setItemText(e.target.value)}} value={itemText}></input>
+          <button className='submitButton' >Summarize</button>
+        </form>
+        <p className='subhead'>Summary</p>
+        <input id="" className="summarybox" type='text' placeholder=''></input>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 
 const PORT =  process.env.PORT || 5500;
+
+app.use(cors());
 
 const TodoItemRoute = require('./routes/todoItems');
 
